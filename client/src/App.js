a9f7a559ad18b9e15444099eb2a6d02483f07819
raphaelkhan8 <raphaelkhan8@gmail.com>
2022-03-14 19:08:03 -0500

    displaying cat facts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [catFacts, setCatFacts] = useState([])
+
+  const getCatFacts = async () => {
+    const { data } = await axios.get('http://localhost:3001/api/getCatFacts')
+    setCatFacts(data.data)
+  }
+
+  useEffect(() => {
+    getCatFacts()
+  }, [])
+
   return (
     <div className="App">
-      <h2>HELLO WORLD!!!</h2>
+      <h2>CAT FACTS!!!</h2>
+      {catFacts.length && catFacts.map(catFact => 
+        (<div>Fact: {catFact.fact}</div>)
+      )}
     </div>
   );
 }
